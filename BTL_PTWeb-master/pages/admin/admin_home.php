a1168a05d055fd78be2cdee9b2dd3b8c115d0fb2
quangdien12-02 <vuquangdien120204@gmail.com>
2026-07-23 20:43:48 +0700

    Delete BTL_PTWeb-master directory

--- BTL_PTWeb-master/pages/admin/admin_home.php
+++
@@ -1,8 +0,0 @@
-<div class="card">
-    <h2>Hệ thống quản lý sinh viên HUMG</h2>
-    <div class="home">
-        <h3>Trường Đại học Mỏ - Địa chất</h3>
-        <p>Trang quản trị dùng để quản lý danh sách sinh viên, khoa, lớp, học phần và điểm học phần.</p>
-        <p>Chọn mục Sinh viên ở thanh menu để thực hiện thêm, xem, sửa và xóa thông tin sinh viên.</p>
-    </div>
-</div>
